09f4577419927ca1a1704fdf17df6a3db960610c
Trung <57174311+trungnt2910@users.noreply.github.com>
2025-04-27 19:38:05 +1000

    chore: Add `RedirectWithError` extension
    This helps with redirecting the user back to the original form to
    display an error message.

--- EzyTaskin/Utils/ControllerExtensions.cs
+++ EzyTaskin/Utils/ControllerExtensions.cs
@@ -32,9 +32,52 @@ public static class ControllerExtensions
         );
     }
 
+    public static RedirectResult RedirectWithError(
+        this ControllerBase controller,
+        string? error = null,
+        string? returnUrl = null,
+        bool includeForm = true
+    )
+    {
+        returnUrl ??= controller.Request.GetTypedHeaders().Referer?.ToString();
+        returnUrl ??= "/";
+
+        if (!controller.ModelState.IsValid)
+        {
+            error ??= controller.ModelState.Values
+                .SelectMany(v => v.Errors)
+                .FirstOrDefault()
+                ?.ErrorMessage;
+        }
+
+        var args = controller.Request.Query
+            .SelectMany(q => q.Value.Select(v => new KeyValuePair<string, object?>(q.Key, v)));
+
+        if (includeForm && controller.Request.HasFormContentType)
+        {
+            var form = controller.Request.Form
+                .SelectMany(f => f.Value.Select(v => new KeyValuePair<string, object?>(f.Key, v)));
+
+            args = args.Concat(form);
+        }
+
+        if (error is not null)
+        {
+            args = args.Concat([ new(nameof(error), error) ]);
+        }
+
+        return controller.RedirectImpl(returnUrl, null, args);
+    }
+
     private static RedirectResult RedirectImpl(this ControllerBase controller,
         string returnUrl, Uri? referrer, IEnumerable<KeyValuePair<string, object?>>? args)
     {
+        if (referrer is not null)
+        {
+            // Requested return is already absolute.
+            returnUrl = new Uri(referrer, returnUrl).ToString();
+        }
+
         if (args is not null)
         {
             returnUrl = QueryHelpers.AddQueryString(returnUrl, args.Select((kvp) =>
@@ -47,15 +90,7 @@ public static class ControllerExtensions
             }));
         }
 
-        // No referrer.
-        if (referrer is null)
-        {
-            return controller.Redirect(returnUrl);
-        }
-
-        // Requested return is already absolute.
-        var returnUri = new Uri(referrer, returnUrl);
-        return controller.Redirect(returnUri.ToString());
+        return controller.Redirect(returnUrl);
     }
 
     public static Guid TryGetAccountId(this ControllerBase controller)
