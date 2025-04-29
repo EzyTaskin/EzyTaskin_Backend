a1eac91bf41c1168dca2d7db678d68d428f88c34
Trung <57174311+trungnt2910@users.noreply.github.com>
2025-04-30 09:06:19 +1000

    fix: Use forwarded headers and HSTS for production
    In the production instance, the cloud provider only serves the service
    on HTTP.
    
    This fix lets the backend look at the forwarded headers to recognize
    the original protocol and generate appropriate redirect URLs.

--- EzyTaskin/Program.cs
+++ EzyTaskin/Program.cs
@@ -147,6 +147,12 @@ if (app.Environment.IsDevelopment())
         options.SwaggerEndpoint("v1/swagger.json", "v1");
     });
 }
+else
+{
+    app.UseForwardedHeaders();
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 
